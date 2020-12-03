29d246768a4959315b7158644f3cf06babdac243
oliver254 <oliver254@hotmail.fr>
2020-12-03 01:41:42 +0100

    #6 : Add git utils

---
+++ src/Monbsoft.UpdateVersion/Core/GitUtils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.CommandLine.Invocation;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monbsoft.UpdateVersion.Core
+{
+    public static class GitUtils
+    {
+        public static async Task<bool> IsInstalled(string workingDir)
+        {
+            try
+            {
+                var result = await Process.ExecuteAsync("git", "--version", workingDir: workingDir);
+                return result == 0;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
